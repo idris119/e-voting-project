6561c4abc60682f411935a1eff2055dd0622c04d
joseph <josephmacharia575@gmail.com>
2022-09-09 14:19:01 +0300

    update

--- e_voting_system/index.js
+++ e_voting_system/index.js
@@ -1,17 +1,21 @@
+const options = {
+    method: 'GET',
+    headers: {
+        'X-RapidAPI-Key': '37a0247b65msh466a208c6da9e68p1a664djsn130bf0d93105',
+        'X-RapidAPI-Host': 'poll-maker.p.rapidapi.com'
+    }
+};
+
+fetch('https://poll-maker.p.rapidapi.com/', options)
+    .then(response => response.json())
+    .then(response => console.log(response))
+    .catch(err => console.error(err));
+    
 let popup =document.getElementById('popup');
-            function openpopup(){
-                popup.classList.add("openpopup")
-
+    function openpopup(){
+         popup.classList.add("openpopup")
+         }
+    function closepopup(){
+        popup.classList.remove("openpopup")
             }
-            function closepopup(){
-                popup.classList.remove("openpopup")
-
-            }
-            function increment(){
-                let i=0;
-                i=+1;
-                return increment();
-            }
-            let votesCasted = document.getElementById('submitId');
-
-                 votesCasted.addEventListener (click => increment(votes-casted))
+            
